169cb624b1099ef9d6df36aecfc205f49a43264b
Henrique Diniz <henriquee.diniz@hotmail.com>
2020-05-25 15:31:21 -0300

    add socialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,12 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/henrique-diniz",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/henriquediniz94/",
+  },
+]
+
+export default links
